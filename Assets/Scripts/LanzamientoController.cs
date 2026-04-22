d31dc2345af20f139796d7b60cfad934da1256b7
GustavoAlfonsin <GustavoAriel.Alfonsin@alumnos.uai.edu.ar>
2026-04-22 10:32:01 -0300

    Hice el script que controla el control principal del juego

---
+++ Assets/Scripts/LanzamientoController.cs
@@ -0,0 +1,146 @@
+using JetBrains.Annotations;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LanzamientoController : MonoBehaviour
+{
+    public enum Estado
+    {
+        Esperando,
+        SeleccionAngulo,
+        SeleccionFuerza,
+        Lanzado
+    }
+
+    public Estado state = Estado.Esperando;
+
+    [Header("Referencias")]
+    public RectTransform flechaUI;
+    public Image barraFuerza;
+    public GameObject poncho;
+
+    [Header("Angulo")]
+    public float velocidadRotacion = 120f;
+    private float anguloActual = 0f;
+    private bool subiendo = true;
+
+    [Header("Fuerza")]
+    public float fuerzaMin = 1f;
+    public float fuerzaMax = 100f;
+    public float velocidadDeCarga = 100f;
+    private float fuerzaActual = 0f;
+    private float multiplicadorFuerza = 0.4f;
+    private bool cargandoSube = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody rb = poncho.GetComponent<Rigidbody>();
+        rb.isKinematic = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (state == Estado.Esperando)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                state = Estado.SeleccionAngulo;
+                flechaUI.gameObject.SetActive(true);
+            }
+        } else if (state == Estado.SeleccionAngulo)
+        {
+            Debug.Log("Esta con el angulo");
+            if (Input.GetMouseButton(0))
+            {
+                actualizarAngulo();
+            }
+
+            if (Input.GetMouseButtonUp(0))
+            {
+                state = Estado.SeleccionFuerza;
+                flechaUI.gameObject.SetActive(false);
+                barraFuerza.gameObject.SetActive(true);
+            }
+        }else if (state == Estado.SeleccionFuerza)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                ActualizarFuerza();
+            }
+
+            if (Input.GetMouseButtonUp(0))
+            {
+                barraFuerza.gameObject.SetActive(false);
+                LanzarPoncho();
+                state = Estado.Lanzado;
+            }
+        }
+    }
+
+    private void LanzarPoncho()
+    {
+        Rigidbody rb = poncho.GetComponent<Rigidbody>();
+        rb.isKinematic = false;
+        float rad = anguloActual * Mathf.Deg2Rad;
+        Vector3 direccion = new Vector3(0, Mathf.Sin(rad), Mathf.Cos(rad));
+        Debug.Log(direccion);
+        poncho.transform.rotation = Quaternion.identity;
+        rb.velocity = direccion * fuerzaActual * multiplicadorFuerza;
+    }
+
+    private void ActualizarFuerza()
+    {
+        if (cargandoSube)
+        {
+            fuerzaActual += velocidadDeCarga * Time.deltaTime;
+        }
+        else
+        {
+            fuerzaActual -= velocidadDeCarga * Time.deltaTime;
+        }
+
+        if (fuerzaActual >= fuerzaMax)
+        {
+            fuerzaActual = fuerzaMax;
+            cargandoSube = false;
+        }
+
+        if (fuerzaActual <= fuerzaMin)
+        {
+            fuerzaActual = fuerzaMin;
+            cargandoSube = true;
+        }
+
+        barraFuerza.fillAmount = fuerzaActual / fuerzaMax;
+    }
+
+    private void actualizarAngulo()
+    {
+        if (subiendo)
+        {
+            anguloActual += velocidadRotacion * Time.deltaTime;
+        }
+        else
+        {
+            anguloActual -= velocidadRotacion * Time.deltaTime;
+        }
+
+        if (anguloActual >= 90f)
+        {
+            anguloActual = 90f;
+            subiendo = false;
+        }
+
+        if (anguloActual <= 0f)
+        {
+            anguloActual = 0f;
+            subiendo = true;
+        }
+        Debug.Log("Angulo: " + anguloActual);
+        flechaUI.localRotation = Quaternion.Euler(0,0,anguloActual);
+    }
+}
